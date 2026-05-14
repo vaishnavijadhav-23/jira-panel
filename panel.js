36a7262b1e3223df5531307fdb7b717e1c257bf0
vaishnavijadhav-23 <vaishnavi.jadhav@amrutsoftware.com>
2026-05-14 12:24:54 +0530

    Add files via upload

---
+++ panel.js
@@ -0,0 +1,14 @@
+console.log("Bridge Context:", AdaptavistBridgeContext.context);
+
+AdaptavistBridge.request({
+  url: `/rest/api/2/issue/${AdaptavistBridgeContext.context.issueKey}`,
+  type: 'GET'
+})
+.then(issue => {
+  document.getElementById("issueInfo").innerText =
+    `${issue.key} is a ${issue.fields.issuetype.name} currently in status "${issue.fields.status.name}"`;
+})
+.catch(err => {
+  console.error("Error fetching issue:", err);
+  document.getElementById("issueInfo").innerText = "Unable to load issue details.";
+});
