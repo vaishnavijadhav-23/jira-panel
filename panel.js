6f038945e8ea300e78fe1d4b26ada499221b303e
vaishnavijadhav-23 <vaishnavi.jadhav@amrutsoftware.com>
2026-05-14 16:18:05 +0530

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,6 +1,7 @@
 try {
 console.log(AdaptavistBridgeContext.context.issueKey);
-
+const issueKey = AdaptavistBridgeContext.context.issueKey;
+console.log("ISSUE key : ",issueKey)
 AdaptavistBridge.request({
     url: `/rest/api/2/issue/${AdaptavistBridgeContext.context.issueKey}`,
     type: 'GET'
