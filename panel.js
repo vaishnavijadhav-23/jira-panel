3da369a256cdb414839c143ed18c01b23b617611
vaishnavijadhav-23 <vaishnavi.jadhav@amrutsoftware.com>
2026-05-15 14:05:05 +0530

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,8 +1,16 @@
-const context = window.AdaptavistBridgeContext.context || {};
-console.log("Bridge Context:", JSON.stringify(context, null, 2));
+// const context = window.AdaptavistBridgeContext.context || {};
+// console.log("Bridge Context:", JSON.stringify(context, null, 2));
 
-console.log("Context : ",AdaptvistBridgeContext.context)
+// console.log("Context : ",AdaptvistBridgeContext.context)
 
-if (!context.issueKey) {
-  console.warn("No issueKey found — panel may not be in an issue view.");
-}
+// if (!context.issueKey) {
+//   console.warn("No issueKey found — panel may not be in an issue view.");
+// }
+AdaptavistBridge.request({
+  url: `/rest/api/2/issue/NP-1`,
+  type: 'GET'
+})
+.then(issue => {
+  document.getElementById("issueInfo").innerText =
+    `${issue.key} (${issue.fields.project.key}) → ${issue.fields.summary}`;
+});
