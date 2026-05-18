108380ddee1e53e2f292d988a51c90911097e3d0
vaishnavijadhav-23 <vaishnavi.jadhav@amrutsoftware.com>
2026-05-18 11:00:15 +0530

    Update panel.js

--- panel.js
+++ panel.js
@@ -12,18 +12,18 @@ console.log("Context : ",context)
 
 console.log("Bridge Context:", AdaptavistBridgeContext.context);
 
-// AdaptavistBridge.request({
-//   url: `/rest/api/2/issue/NP-1`,
-//   type: 'GET'
-// })
+AdaptavistBridge.request({
+  url: `/rest/api/2/issue/NP-1`,
+  type: 'GET'
+})
   
-// .then(issue => {
-//   // Correct property: issue.fields.issuetype.name
-//   document.getElementById("issueInfo").innerText =
-//     `${issue.key} is a ${issue.fields.issuetype.name} currently in status "${issue.fields.status.name}"`;
-// })
-// .catch(err => {
-//   console.error("Error fetching issue:", err);
-//   document.getElementById("issueInfo").innerText = "Unable to load issue details.";
-// });
+.then(issue => {
+  // Correct property: issue.fields.issuetype.name
+  document.getElementById("issueInfo").innerText =
+    `${issue.key} is a ${issue.fields.issuetype.name} currently in status "${issue.fields.status.name}"`;
+})
+.catch(err => {
+  console.error("Error fetching issue:", err);
+  document.getElementById("issueInfo").innerText = "Unable to load issue details.";
+});
 
