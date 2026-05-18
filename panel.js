944e89bbd7e39f3fe0492c06b897b61d97949ab8
vaishnavijadhav-23 <vaishnavi.jadhav@amrutsoftware.com>
2026-05-18 15:58:43 +0530

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,5 +1,5 @@
 const context=window.AdaptavistBridgeContext.context;
-console.log("Context : ",context)
+console.log("Context : ",window.AdaptavistBridgeContext)
 
 console.log("Bridge Context:", AdaptavistBridgeContext.context);
 
