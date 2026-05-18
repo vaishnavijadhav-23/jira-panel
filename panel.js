218e995c6f4529b5518379910198dd5a7ccfd0ee
vaishnavijadhav-23 <vaishnavi.jadhav@amrutsoftware.com>
2026-05-18 15:36:54 +0530

    Update panel.js

--- panel.js
+++ panel.js
@@ -4,14 +4,14 @@ console.log("Context : ",context)
 console.log("Bridge Context:", AdaptavistBridgeContext.context);
 
 AdaptavistBridge.request({
-  url: `/rest/api/2/issue/NP-1`,
+  url: `/rest/api/2/issue/${AdaptavistBridgeContext.context.issueKey}`,
   type: 'GET'
 })
   
 .then(issue => {
   // Correct property: issue.fields.issuetype.name
   document.getElementById("issueInfo").innerText =
-    `${issue.key} is a ${issue.fields.issuetype.name} currently in status "${issue.fields.status.name}"`;
+`${issue.key} is a ${issue.fields.issuetype.name} currently in status "${issue.fields.status.name}"`;
 })
 .catch(err => {
   console.error("Error fetching issue:", err);
