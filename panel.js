efeed9baba88e401c423e99e5c490640d86434ef
vaishnavijadhav-23 <vaishnavi.jadhav@amrutsoftware.com>
2026-05-20 12:01:01 +0530

    Update panel.js

--- panel.js
+++ panel.js
@@ -2,8 +2,8 @@ console.log("Context: ", AdaptavistBridgeContext.context);
 
 const issueKey = AdaptavistBridgeContext.context.issueKey;
 const projectKey = AdaptavistBridgeContext.context.projectKey;
-const location = AdaptavistBridgeContext.context.location;
+const Clocation = AdaptavistBridgeContext.context.location;
 
 console.log('issueKey:', issueKey);
 console.log('projectKey:', projectKey);
-console.log('location:', location);
+console.log('location:', Clocation);
