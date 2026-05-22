0e4007dc5f9d42fef40532962248714d74eb8fe8
vaishnavijadhav-23 <vaishnavi.jadhav@amrutsoftware.com>
2026-05-22 12:18:24 +0530

    Update panel.js

--- panel.js
+++ panel.js
@@ -5,12 +5,8 @@
         type: 'GET'
     })
     .then(issue => {
-       baseurl=issue.self;
-        jiraBaseUrl = issue.self.split("/rest/api")[0];
-        issuekey=issue.key;
-        issuesummary=issue.fields.summary;
+      
         console.log("Issue response:", issue);
-        console.log("BaseUrl : ",jiraBaseUrl)
     })
     .catch(error => {
         console.error("API Error:", error);
